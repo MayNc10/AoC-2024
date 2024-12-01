279261dfebc6e77930d01429090ee686f7b4e5e9
May Neelon <mayflowerc10@gmail.com>
2024-12-01 00:28:05 -0500

    Add day 1 in kotlin

--- day1.kt
+++ day1.kt
@@ -1,10 +1,19 @@
+import java.io.File
+import kotlin.math.abs
+
 fun main() {
-    println("Hello, World!")
-    val list = listOf("hi", "world", "this", "is", "may")
-    do_thing(list)
-    
+    val lines = File("input/day1.txt").readText().lines().map { s -> s.split("\\s+".toRegex())}
+    val list1 = lines.map{s -> s[0].toInt()}.sorted()
+    val list2 = lines.map{s -> s[1].toInt()}.sorted()
+    part2(list1, list2)
 }
 
-fun do_thing(thing: List<String>) {
-    thing.forEach { i: String -> println(i) }
+fun part1(list1: List<Int>, list2: List<Int>) {
+    val sum = list1.withIndex().fold(0) { acc, item: IndexedValue<Int> -> acc + abs(item.value - list2[item.index]) }
+    println(sum)
+}
+
+fun part2(list1: List<Int>, list2: List<Int>) {
+    val sum = list1.fold(0) { acc, item: Int -> acc + item * list2.count{it == item}}
+    println(sum)
 }
