e57101fd874681cfe043fff50af110951e7c5fb6
May <MayflowerC10@gmail.com>
2024-12-02 02:56:18 -0500

    Add haskell day 2

---
+++ haskell/day2.hs
@@ -0,0 +1,34 @@
+import Data.List
+import Debug.Trace
+import System.IO
+import Control.Monad
+
+main = do
+    contents <- readFile "input/day2.txt"
+    let line_arr = map (map read . words) (lines contents)
+    print (part1 line_arr)
+    print (part2 line_arr)
+
+part1 :: [[Int]] -> Int
+part1 line_arr = length (filter id (map isSafe line_arr))
+
+part2 :: [[Int]] -> Int
+part2 line_arr = length (filter id (map isSafeDamp line_arr))
+
+isSafeDamp :: [Int] -> Bool
+isSafeDamp list = foldl (\acc combo -> acc || isSafe combo) False (map (`deleteAt` list) [0.. (length list)])
+
+isSafe :: [Int] -> Bool
+isSafe list = foldl (\acc item -> acc && isItemSafe item (pairDiff (head windowList)) ) True windowList
+        where windowList = zip list (tail list)
+
+isItemSafe :: (Int, Int) -> Int -> Bool
+isItemSafe pair first = abs (pairDiff pair) >= 1 && abs (pairDiff pair) <= 3 && signum (pairDiff pair) == signum first
+
+pairDiff :: (Int, Int) -> Int
+pairDiff = uncurry (-)
+
+deleteAt :: Int -> [Int] -> [Int]
+deleteAt idx xs | not (null rgt) = lft ++ tail rgt
+                | otherwise = lft
+  where (lft, rgt) = splitAt idx xs
